057c18993f7eada2a96a3712bcc6309f9180466b
rehanulhb <rehanulh@gmail.com>
2024-11-24 12:31:45 +0100

    Working on Featured Jobs Portion, show jobs based on dataLength

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -3,6 +3,8 @@ import Job from "../Job/Job";
 
 const FeaturedJobs = () => {
   const [jobs, setJobs] = useState([]);
+  //Load show all data
+  const [dataLength, setDataLength] = useState(4);
 
   useEffect(() => {
     fetch("jobs.json")
@@ -21,8 +23,11 @@ const FeaturedJobs = () => {
       </div>
       <div className="grid grid-cols-2 gap-6">
         {
-            jobs.map(job => <Job key={job.id} job={job}></Job>)
+            jobs.slice(0, dataLength).map(job => <Job key={job.id} job={job}></Job>)
         }
+      </div>
+      <div className={dataLength=== jobs.length ? 'hidden' : ''}>
+        <button onClick={()=> setDataLength(jobs.length)} className="btn btn-primary">Show all jobs</button>
       </div>
     </div>
   );
--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,3 +1,6 @@
+import { MdLocationOn } from "react-icons/md";
+import { AiOutlineDollar } from "react-icons/ai";
+
 const Job = ({ job }) => {
   const { logo, job_title, company_name, remote_or_onsite, location, job_type, salary } = job;
 
@@ -16,8 +19,12 @@ const Job = ({ job }) => {
             <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] mr-4 text-[#7E90FE]">{remote_or_onsite}</button>
             <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] mr-4 text-[#7E90FE]">{job_type}</button>
         </div>
+        <div className="mt-4 flex ">
+            <h2 className="flex mr-4"><MdLocationOn className="text-2xl mr-2"></MdLocationOn>{location}</h2>
+            <h2 className="flex"><AiOutlineDollar className="text-2xl mr-2"></AiOutlineDollar>{salary}</h2>
+        </div>
         <div className="card-actions ">
-          <button className="btn btn-primary">View Details</button>
+          <button className="btn btn-primary bg-gradient-to-r from-cyan-500 to-blue-500">View Details</button>
         </div>
       </div>
     </div>
